ecc16465ef43f516fced2f84d59d93f5e675c822
asiguqa <asiguqa@student.wethinkcode.co.za>
2020-09-09 08:31:12 +0200

    wtc-lms save

--- robot.py
+++ robot.py
@@ -1,45 +1,49 @@
 
 
 # TODO: Decompose into functions
+def move_forward(length):
+    print("* Move Forward "+str(length))
+
+def turn_right(degrees):
+    print("* Turn Right "+str(degrees)+" degrees")
 def move_square():
     size = 10
     print("Moving in a square of size "+str(size))
     for i in range(4):
         degrees = 90
-        print("* Move Forward "+str(size))
-        print("* Turn Right "+str(degrees)+" degrees")
-
+        move_forward(size)
+        turn_right(degrees)
 def move_rectangle():
     length = 20
     width = 10
     print("Moving in a rectangle of "+str(length)+" by "+str(width))
     for i in range(2):
         degrees = 90
-        print("* Move Forward "+str(length))
-        print("* Turn Right "+str(degrees)+" degrees")
-        print("* Move Forward "+str(width))
-        print("* Turn Right "+str(degrees)+" degrees")
+        move_forward(length)
+        turn_right(degrees)
+        move_forward(width)
+        turn_right(degrees)
 
 def move_circle():
     print("Moving in a circle")
     degrees = 1
     for i in range(360):
         length = 1
-        print("* Move Forward "+str(length))
-        print("* Turn Right "+str(degrees)+" degrees")
+        move_forward(length)
+        turn_right(degrees)
 
 def square_dancing():
     print("Square dancing - 3 squares of size 20")
+    length = 20
+    size = 20
+    degrees = 90
     for i in range(3):
-        length = 20
-        print("* Move Forward "+str(length))
+        move_forward(length)
         print("Moving in a square of size 20")
-        size = 20
         for j in range(4):
-            degrees = 90
-            print("* Move Forward " + str(size))
-            print("* Turn Right " + str(degrees) + " degrees")
-
+            move_forward(size)
+            turn_right(degrees)
+            
 def crop_circle():
     print("Crop circles - 4 circles")
     length = 20
@@ -49,11 +53,9 @@ def crop_circle():
         print("Moving in a circle")
         length = 1
         for k in range(360):
-            print("* Move Forward " + str(length))
-            print("* Turn Right " + str(degrees) + " degrees")
-
-
-
+            move_forward(length)
+            turn_right(degrees)
+            
 def move():
     '''
     move_in_shapes()
